3741956b2f1201931bd9684a34bd6dd25797a670
Divvy C. <54956345+DivvyCr@users.noreply.github.com>
2022-07-16 12:45:45 +0100

    Use command-line arguments for replay paths.

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
+import os
+import sys
+
 from boxcars_py import parse_replay
 
 from replay_objects.replay import Replay
 
 if __name__ == '__main__':
-    with open("replays/BFB0F4B611EB17CB17D5629553218CFC.replay", "rb") as f:
+    replay_path = sys.argv[1]
+    if not os.path.exists(replay_path):
+        print("Invalid path!")
+        sys.exit()
+
+    with open(replay_path, "rb") as f:
         buf = f.read()
         f.close()
 
@@ -13,3 +21,7 @@ if __name__ == '__main__':
     players = replay.get_players()
     ball = replay.get_ball()
     game_info = replay.get_game_info()
+
+    print(game_info.get_dataframe()
+          .iloc[::10, :] # Every 10th entry (frame).
+          .to_string())
